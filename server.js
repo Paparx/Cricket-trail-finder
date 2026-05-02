8b12065760cc358394f1a0fc17602993e67e4dfc
Paparx <abhirajarya9934@gmail.com>
2026-05-02 21:40:01 +0530

    changes according to deploy on railway

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.get('/api/health', (req, res) => {
 });
 
 // For any other request, serve index.html (supporting SPA routing if added later)
-app.get('*', (req, res) => {
+app.get((req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
